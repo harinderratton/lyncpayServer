49c05ed5eee9b49568bb20741ab7eb57d8cb7f99
Your Name <you@example.com>
2021-04-16 14:49:50 +0530

    latest

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -29,7 +29,7 @@ app.route('/getMyPaymethods').post(payMethods.getMyPaymethods)
 
 //general data controller routes start
 app.route('/getLyncpayUsers').post(dataControllerMethods.getLyncpayUsers)
-app.route('/updateUserProfileData/:email/:password/:id').post(dataControllerMethods.updateUserProfileData)
+app.route('/updateUserProfileData/:phone/:email/:id').post(dataControllerMethods.updateUserProfileData)
  
 
 };
