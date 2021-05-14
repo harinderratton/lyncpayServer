b0e1f58a341ec31c67c129aeabb1e3e8b91d4d3b
Your Name <you@example.com>
2021-05-14 16:39:46 +0530

    latest

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -143,7 +143,7 @@ async function Admin_fetchAllUsers(req, res, next) {
 	try {
 
        var userList = await UserTable.find()
-       return res.status(401).send({ status: true, data: userList});
+       return res.json({status: true, data: userList});
  
   
 	} catch (err) {
