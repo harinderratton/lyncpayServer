7382b2864c67bbdd397f966cb13736e78adb467f
Your Name <you@example.com>
2021-06-01 15:30:32 +0530

    latest

--- api/models/dynamicDataModel.js
+++ api/models/dynamicDataModel.js
@@ -2,11 +2,11 @@
 
 var mongoose = require('mongoose');
 
-var dynamicData = new mongoose.Schema({
+var dynamicDataSchema = new mongoose.Schema({
 	title: {type: String, default:null},
     desc:  {type: String, required : true},
 
 }, {timestamps: true});
 
-module.exports = mongoose.model('DynamicDataTable', dynamicData);
+module.exports = mongoose.model('DynamicDataTable', dynamicDataSchema);
  
