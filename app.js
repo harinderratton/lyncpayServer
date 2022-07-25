c90b9ce9f2c6fc27729dbb1e2586e55665281048
Your Name <harinder.cipio.ai>
2022-07-25 11:54:15 +0530

    latest

--- app.js
+++ app.js
@@ -1,39 +1,44 @@
-var express = require('express'),
+var express = require("express"),
   app = express(),
-  port = process.env.PORT || 3002,
-  mongoose = require('mongoose'),
-  users = require('./api/models/UsersModel'),
-  groups = require('./api/models/groupModel'),
-  payMethod = require('./api/models/payMethodModel'),
-  adminMethod = require('./api/models/adminModel'),
-  dynamicData = require('./api/models/dynamicDataModel'),
-  friends = require('./api/models/friendsModel'),
-  bodyParser = require('body-parser'),
-   multer  = require('multer');
-  
+  port = 3001,
+  mongoose = require("mongoose"),
+  users = require("./api/models/UsersModel"),
+  groups = require("./api/models/groupModel"),
+  payMethod = require("./api/models/payMethodModel"),
+  adminMethod = require("./api/models/adminModel"),
+  dynamicData = require("./api/models/dynamicDataModel"),
+  friends = require("./api/models/friendsModel"),
+  bodyParser = require("body-parser"),
+  multer = require("multer");
+
 //mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/'); // live
-mongoose.connect('mongodb://localhost/lyncpay', { useMongoClient: true }); // local
+mongoose.connect("mongodb://localhost/"); // live
+mongoose.connect("mongodb://localhost/lyncpay", { useMongoClient: true }); // local
 var path = __dirname;
-app.use('/server/data', express.static(path + '/data'));
+app.use("/server/data", express.static(path + "/data"));
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Auth_Token');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-    
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type,Auth_Token"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set('port', port);
+app.set("port", port);
 
-var routes = require('./api/routes/Routes');
+var routes = require("./api/routes/Routes");
 routes(app);
 
 app.listen(port);
 module.exports = app;
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log("todo list RESTful API server started on: " + port);
